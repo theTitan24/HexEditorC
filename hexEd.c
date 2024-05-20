2f02d93f331b1aa7d98102b8ba1301d59b089c3d
TheTitan24 <76175416+theTitan24@users.noreply.github.com>
2024-05-20 12:24:51 +0530

    Added Features
    Added New Features:
    1, Help Window
    2. Edit functionality
    3. Save Option
    4. List the Whole Program
    6. Display the text contents
    7. Overall UI Improved
    8. Some Error Checking and Restrictions added
    
    --TBD--
    Increase memory size to more than 1024 bytes more efficiently and dynamically
    Any help in this regard will be highly appreciated

--- hexEd.c
+++ hexEd.c
@@ -13,7 +13,12 @@ int printText(unsigned char *buffer, int bytes) {
 
 int printHex(unsigned char *buffer, int numOfBytes, int offset) {
 	int prevI = 0;
-	printf("          0  1  2  3  4  5  6  7  8  9  a  b  c  d  e  f ");
+	printf("Offset:   ");
+	
+	for(int uI = 0; uI < 16; uI++) {
+		printf("%.2x ", uI);
+	}
+	
 	for(int i = 0; i < numOfBytes; i++) {
 		if(i % 16 == 0) {
 			if(i > 0) {
@@ -30,6 +35,12 @@ int printHex(unsigned char *buffer, int numOfBytes, int offset) {
 	printf("\n");
 }
 
+int editHex(unsigned char *buffer, int  numOfBytes) {
+	for(int i = 0; i < numOfBytes; i++) {
+		scanf("%hhx", buffer + i);
+	}
+}
+
 int main(int argc, char** argv) {
 
 	FILE *file = fopen(argv[1], "r");
@@ -38,45 +49,98 @@ int main(int argc, char** argv) {
 		return(1);
 	}
 
-	int size = 1024;
-/*
-	// To be implemented
-	long size;
-	fseek(file, 0, SEEK_END);
-	size = ftell(file);
-*/	
+	int size = 1024;	
 
 	unsigned char buffer[size];
 	int numOfBytes = fread(buffer, 1, size, file);		
 
 	printHex(buffer, numOfBytes, 0);
-
+	printf("\n");
+	
 	// Now Let's listen for use input
 	char cmd;
-	unsigned int loc;
+	int loc;
 	
 	while(true) {
 		scanf("%c %x", &cmd, &loc);
 
 		switch(cmd) {
+			case 'h':
+				printf("Command Syntax: [command] [memory address] \n");
+				printf("\nSupported Commands: \n");
+				printf("h : Prints this page \n");
+				printf("l : Prints the rest of the hex from the given address. \n");
+				printf("p : Prints 16 bytes from the given address. \n");
+				printf("w : Change specified bytes of the file. \n");			
+				printf("t : Displays the text contents of the given address \n");
+				printf("x : Exit without saving changes (Use Memory address as 0) \n");
+				printf("s : Exit saving changes (Use Memory address as 0) \n");
+							
+				printf("\n");
+
+				break;
+				
+			case 'l':
+				// Prints the entire Hex of the file
+				printHex(buffer, numOfBytes - loc, loc);
+				printf("\n");
+
+				break;
+			
 			case 'p':
 				// Prints 16 bytes from the given address
 				printHex(buffer + loc, 16, loc);
+				printf("\n");
+					
+				break;
+
+			case 'w':
+				// Change specified bytes of the file
+				int wBytes;
+				
+				printf("How Many Bytes? " );
+				scanf("%d", &wBytes);
+				
+				editHex(buffer + loc, wBytes);
+
+				if(wBytes >= 16){
+					printHex(buffer + loc, wBytes, loc);
+				}
+				else {
+					printHex(buffer + loc, 16, loc);
+				}
+				printf("\n");
+				
 				break;
 
 			case 't':
 				// Prints the text content from the given address
 				int n = 16;
-				printf("How many bytes? ");
-				scanf("%d",&n);
+
+				printf("How Many Bytes? ");
+				scanf("%d", &n);
+				
 				printText(buffer + loc, n);
 				printf("\n");
+				
 				break;
 
 			case 'x':
-				// Exits the program
+				// Exits the program without saving changes
 				fclose(file);
+				
 				return 0;
+
+			case 's':
+				// Exits the program after saving changes
+				fclose(file);
+				
+				file = fopen(argv[1], "w");
+				fwrite(buffer, 1, numOfBytes, file);
+				fclose(file);
+			
+				return 0;
+				 
 		}
 	}
 }
